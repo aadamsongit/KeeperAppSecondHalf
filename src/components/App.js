5539732ebd3c4bc3f14107be62c0edbc370e80de
Amber Adamson <88444100+aadamsongit@users.noreply.github.com>
2023-02-10 14:39:49 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNotesMap(noteItem) {
+  return (
+    <Note key={noteItem.id} title={noteItem.title} content={noteItem.content} />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(createNotesMap)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
